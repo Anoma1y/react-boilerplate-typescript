c3a2c0c3ffb388cfcac1d01abe1d9f8c1b6169d6
German <ueuknjg@gmail.com>
2018-11-19 19:54:50 +0300

    fix

--- app/App.js
+++ app/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Image from './components/Image';
 
 @connect(({ router }) => ({ router }))
 export default class App extends Component {
@@ -9,6 +10,7 @@ export default class App extends Component {
   render() {
     return (
       <div className={'page'}>
+        <Image src={'https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg'} alt={"qwe"}/>
         <h1>Hello World</h1>
       </div>
     );
--- app/components/Image/index.js
+++ app/components/Image/index.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default (props) => <img className={props.className} src={props.src} alt={props.alt} />
+const Image = (props) => <img className={props.className} src={props.src} alt={props.alt} />
+const fuck = '10'
 
+const StyledImage = styled(Image)`
+  height: ${fuck}px;
+  width: 50%;
+`;
+
+export default StyledImage;
