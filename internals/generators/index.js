abaa423544d93f98949b057d50f04d9637ce6f4b
Anoma1y <ueuknjg@gmail.com>
2018-12-29 16:35:23 +0300

    fix

--- internals/generators/index.js
+++ internals/generators/index.js
@@ -4,11 +4,9 @@ const { exec } = require('child_process');
 const componentsGenerate = require('./components/index.js');
 const containerssGenerate = require('./containers/index.js');
 const storeGenerate = require('./store/index.js');
-const testsGenerate = require('./tests/index.js');
 
 module.exports = (plop) => {
   plop.setGenerator('Component', componentsGenerate);
   plop.setGenerator('Container', containerssGenerate);
   plop.setGenerator('Store', storeGenerate);
-  plop.setGenerator('Test', testsGenerate);
 };
