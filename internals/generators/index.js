44546af930ff01a6e31b6818a2adcf9f92b48d37
German Voytekhovich <ueuknjg@gmail.com>
2019-01-09 00:01:18 +0300

    remove store and fix components generator

--- internals/generators/index.js
+++ internals/generators/index.js
@@ -3,10 +3,8 @@ const path = require('path');
 const { exec } = require('child_process');
 const componentsGenerate = require('./components/index.js');
 const containerssGenerate = require('./containers/index.js');
-const storeGenerate = require('./store/index.js');
 
 module.exports = (plop) => {
   plop.setGenerator('Component', componentsGenerate);
   plop.setGenerator('Container', containerssGenerate);
-  plop.setGenerator('Store', storeGenerate);
 };
