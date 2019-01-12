ba5514165c63c4e4f0a47fbb82be387f35b27202
German Voytekhovich <ueuknjg@gmail.com>
2019-01-12 22:15:28 +0300

    add test for urls and fix parse params (add break from loop if key || value is undefined and return empty object) and serialize params (remove lodash function isArray)

--- app/utils/urls.ts
+++ app/utils/urls.ts
@@ -1,26 +1,5 @@
-import _ from 'lodash';
-
 const isValidURL = (str: string): boolean => {
-  const pattern = new RegExp(
-    '^(?:(?:https?|ftp)://)' +
-    '(?:\\S+(?::\\S*)?@)?' +
-    '(?:' +
-    '(?!(?:10|127)(?:\\.\\d{1,3}){3})' +
-    '(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})' +
-    '(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})' +
-    '(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])' +
-    '(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}' +
-    '(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))' +
-    '|' +
-    '(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)' +
-    '(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*' +
-    '(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))' +
-    '\\.?' +
-    ')' +
-    '(?::\\d{2,5})?' +
-    '(?:[/?#]\\S*)?' +
-    '$', 'i',
-  );
+  const pattern = new RegExp(/^((?:(ht|f)tp(s)?:\/\/)|((\/\/)))?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:\/?#[\]@!\$&'\(\)\*\+,;=.]+$/g);
   return pattern.test(str);
 };
 
@@ -28,9 +7,17 @@ const parseParams = (str: string): object => {
   const query = {};
   const pairs = (str[0] === '?' ? str.substr(1) : str).split('&');
 
-  for (let i = 0; i < pairs.length; i++) {
-    const pair = pairs[i].split('=');
-    query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+  if (str.length !== 0) {
+    for (let i = 0; i < pairs.length; i++) {
+      const pair = pairs[i].split('=');
+      const [ key, val ] = pair;
+
+      if (!key || !val) {
+        break;
+      }
+
+      query[decodeURIComponent(key)] = decodeURIComponent(val);
+    }
   }
 
   return query;
@@ -41,7 +28,7 @@ const serializeParams = (obj: object): string => {
 
   for (const p in obj) {
     if (obj.hasOwnProperty(p)) {
-      if (_.isArray(obj[p])) {
+      if (Array.isArray(obj[p])) {
         str.push(`${encodeURIComponent(p)}=${obj[p]}`);
         continue;
       }
